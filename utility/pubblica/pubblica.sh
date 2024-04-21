1cd8bae3a2fac1fa624b14008e3ffbb4e4310f0a
francescobos <81432033+francescobos@users.noreply.github.com>
2024-04-21 18:42:53 +0200

    + backup ion script pubblica.sh

--- utility/pubblica/pubblica.sh
+++ utility/pubblica/pubblica.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 #genera sitemap xml
+backupRemoteSite.sh fboschetti.it
 npx sitemap --index-base-url https://www.fboschetti.it/ < ../../routes/urlDelSito.txt > ../../public/sitemap.xml
 rsync -rzv --delete --exclude-from='escludi.txt' ../../../fboschetti.it/ francesco@206.189.61.36:/home/francesco/fboschetti.it/
-ssh francesco@206.189.61.36 'nvm use 18; pm2 --update-env restart fboschetti; sleep 4; pm2 list'
+ssh francesco@206.189.61.36 'nvm use 20; pm2 --update-env restart fboschetti; sleep 4; pm2 list'
